e654356e8adc408c32e18d6216932b16918a4e36
grahamschock <gschock@gwu.edu>
2020-02-17 17:34:11 -0500

    added semaphore (kinda... not really)

--- composite/src/components/implementation/tests/unit_schedtests/host-ops.c
+++ composite/src/components/implementation/tests/unit_schedtests/host-ops.c
@@ -1,4 +1,5 @@
 #include <assert.h>
+#include <cos_types.h>
 
 //TODO: Ask gabe what we should use
 static void print(const char *str, int len)
@@ -11,9 +12,29 @@ static void panic(void)
   assert(0);
 }
 
+struct lkl_sem {
+  unsigned int size;
+  unsigned int read;
+  unsigned write;
+  CAP_THD * threads;
+}
+
+  
+
+
 static struct lkl_sem *sem_alloc(int count)
 {
-  assert(0);
+
+  lkl_sem* semPtr;
+  
+  cos_meminfo_alloc(struct cos_compinfo *ci, semPtr, sizeof(lkl_sem));
+
+  cos_meminfo_alloc(struct cos_compinfo *ci, semPtr->threads, sizeof(CAP_THD)*count);
+
+  
+  return semPtr;
+
 }
+
 
 
